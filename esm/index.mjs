e7d43d53834d5c7361155112256ddd4d2aa12890
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-08-30 12:47:26 -0600

    Fix import syntax and reorder exports in index.mjs

--- esm/index.mjs
+++ esm/index.mjs
@@ -1,13 +1,11 @@
-import ytdl from "../lib/index.js";
-
+import ytdl from '../lib/index.js';
 export default ytdl;
-
 export const getBasicInfo = ytdl.getBasicInfo;
 export const getInfo = ytdl.getInfo;
 export const chooseFormat = ytdl.chooseFormat;
 export const filterFormats = ytdl.filterFormats;
-export const validateID = ytdl.validateID;
 export const validateURL = ytdl.validateURL;
+export const validateID = ytdl.validateID;
 export const getURLVideoID = ytdl.getURLVideoID;
 export const getVideoID = ytdl.getVideoID;
 export const createAgent = ytdl.createAgent;
@@ -16,6 +14,5 @@ export const downloadFromInfo = ytdl.downloadFromInfo;
 export const downloadFromQuery = ytdl.downloadFromQuery;
 export const search = ytdl.search;
 export const searchOne = ytdl.searchOne;
-export const cache = ytdl.cache;
 export const errors = ytdl.errors;
-export const version = ytdl.version;
+export const cache = ytdl.cache;
