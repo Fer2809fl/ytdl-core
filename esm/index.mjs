29c516c45cfc28d9f99ef593f89897f30f303f24
𝕱𝖊𝖗𝖓𝖆𝖓𝖉𝖔 <179016253+Fer2809fl@users.noreply.github.com>
2026-08-30 11:16:29 -0600

    subida

---
+++ esm/index.mjs
@@ -0,0 +1,18 @@
+import ytdl from "../lib/index.js";
+
+export default ytdl;
+
+export const getBasicInfo = ytdl.getBasicInfo;
+export const getInfo = ytdl.getInfo;
+export const chooseFormat = ytdl.chooseFormat;
+export const filterFormats = ytdl.filterFormats;
+export const validateID = ytdl.validateID;
+export const validateURL = ytdl.validateURL;
+export const getURLVideoID = ytdl.getURLVideoID;
+export const getVideoID = ytdl.getVideoID;
+export const createAgent = ytdl.createAgent;
+export const createProxyAgent = ytdl.createProxyAgent;
+export const downloadFromInfo = ytdl.downloadFromInfo;
+export const cache = ytdl.cache;
+export const errors = ytdl.errors;
+export const version = ytdl.version;
